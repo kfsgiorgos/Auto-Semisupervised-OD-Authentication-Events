3ddea30363a91cdd7765e75e2187b963eb87e89c
Georgios Kaiafas <kfsgiorgos@users.noreply.github.com>
2019-06-03 13:27:13 +0200

    add more comments &  add remove unnecessary code

--- run-logisitcPCA.R
+++ run-logisitcPCA.R
@@ -1,25 +1,22 @@
+# Read the dataset with all users
 ResultMerge <- fread("ResultAllUsers.csv")
 
-LabelsAllUsers <- copy(ResultMerge[, .(TimeInSeconds, Source_User, 
-	Destination_Comp, Label, Time)])
-LabelsAllUsers[, nRowIndex:= 1:.N]
-## Decrease the dataset to make the Onehot Enoding feasible
-LabelsAllUsers[4500000:4650000, .N, by = "Label"]
-
+# Subset columns. Time will not be included in our analysis
 subsetColsAll <- copy(ResultMerge[, !c("Time", "TimeInSeconds", 
 	"Label", "dayTime")])
 SubsetColsRows <- copy(subsetColsAll[4500000:4650000])
 SubsetColsRows[, names(SubsetColsRows)[c(1:10)]:= lapply(.SD, 
 	function(x) as.factor(as.numeric(x))), .SDcols = c(1:10)]
-str(SubsetColsRows)
-
+# Do one-hot-encoding
 system.time(DT_Onehot <- one_hot(SubsetColsRows))
 ## Check if we have 0 or 1 in some columns for all rows in order to exclude those columns
 temp <- transpose(DT_Onehot[, lapply(.SD, sum), .SDcols = names(DT_Onehot)])
 temp[, colNames:= names(DT_Onehot[, lapply(.SD, sum), 
 	.SDcols = names(DT_Onehot)])]
 temp[V1 == 0 | V1 == dim(temp)[1]]
- 
+# Apply Logistic PCA. 
+# A proposed rule of thumb for selecting the number of components is to keep 
+# reduce the dimansionality of DT_Onehot by 2/3.  
 logPCA_result <- logisticPCA(DT_Onehot, k = 900, m = 14, max_iters = 5000,
 	partial_decomp = TRUE, random_start = TRUE)
 saveRDS(logPCA_result, "LogisticPCA_900k_14ms.rds")
