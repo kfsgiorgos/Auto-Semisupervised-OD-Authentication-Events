be3572581b7aba84cc694fd69176c04b4b4fb5fd
Georgios Kaiafas <kfsgiorgos@users.noreply.github.com>
2019-06-03 13:09:21 +0200

    add more comments to create-datasample.R

--- create-datasample.R
+++ create-datasample.R
@@ -1,3 +1,4 @@
+# install & load the required packages
 pkg <- c("ggplot2", "logisticPCA", "lubridate", "foreach", "data.table", 
 	"rARPACK", "dplyr", "mltools", "tidyverse", "igragh")
 new.pkg <- pkg[!(pkg %in% installed.packages())]
@@ -8,14 +9,6 @@ if (length(new.pkg)) {
   lapply(pkg, require, character.only = TRUE)
 }
 
-
-library(data.table)
-library(foreach)
-library(ggplot2)
-library(lubridate)
-library(logisticPCA)
-library(rARPACK)
-library(mltools)
 options(scipen = 9999)
 
 # Red Team Dataset
@@ -26,7 +19,7 @@ setnames(redTeamUnique, c("V1", "V2", "V3", "V4", "V5"), c("TimeInSeconds", "Sou
 	"Destination_Comp", "Time"))
 redTeamUnique[, Label := "Malicious"]
 
-
+# Read the datasets we have already created for each user
 # ----- Normal Users -----
 U22 <- fread("user22.txt", nThread = 40)
 U22 <- U22[V1 < 2600000]
@@ -45,7 +38,6 @@ C830 <- C830[V1 < 2600000]
 
 U228 <- fread("U228DOM1_Norm.txt", nThread = 40)
 U228 <- U228[V1 < 2600000]
-
 NormalUsers <- rbindlist(list(U22, Anonymous, C480, U194, C830, U228))
 setkey(NormalUsers, V1)
 
@@ -67,7 +59,7 @@ U293 <- U293[V1 < 2600000]
 
 MaliciousUsers <- data.table::rbindlist(list(U3005, U66, U737, U748, U293))
 setkey(MaliciousUsers, V1)
-## All users dataset and order by Time
+## Combine NormalUsers & MaliciousUsers & then order by Time
 AllUsers <- data.table::rbindlist(list(NormalUsers, MaliciousUsers))
 setkey(AllUsers, V1)
 
@@ -78,26 +70,26 @@ data.table::setnames(AllUsers, names(AllUsers),
 	"LogonType", "Authnetication_Orientation", 
 	"Success_Failure"))
 
-
 AllUsers[, Time := lubridate::as_datetime(TimeInSeconds)]
 AllUsers[, dayTime := day(Time)]
+# order by Time both datasets
 setkey(AllUsers, TimeInSeconds, Source_User, Source_Comp, Destination_Comp)
 setkey(redTeamUnique, TimeInSeconds, Source_User, Source_Comp, Destination_Comp)
-perform the join using the merge function
+# join using the merge function to label the resuted dataset
 ResultMerge <- merge(AllUsers, redTeamUnique, all.x=TRUE)
 
 
 MalUsers <- c("U3005@DOM1", "U66@DOM1", "U737@DOM1", "U748@DOM1", "U293@DOM1")
 redTeamUnique[Source_User %in% MalUsers]
-### ---- Check if the merge was correct
+### ---- Check if the merge was correct based on the redTeam dataset
 # Events that belong to RedTeam but do not exist in the Authentication Usage Dataset
 UU <- merge(redTeamUnique[Source_User %in% MalUsers], ResultMerge[Label == "Malicious"], all.x = T)
 UU[is.na(Label.y)]
 ## Those are missing as Malicious
 ResultMerge[TimeInSeconds %in% c(752613, 758915) & Source_User == "U737@DOM1" & Source_Comp == "C17693"]
 ### Fix it
-ResultMerge[TimeInSeconds %in% c(752613, 758915) & Source_User == "U737@DOM1" & Source_Comp == "C17693", 
-Label:= "Malicious"]
+ResultMerge[TimeInSeconds %in% c(752613, 758915) & Source_User == "U737@DOM1" & 
+	    Source_Comp == "C17693", Label:= "Malicious"]
 ### Check again if everything is Correct
 dim(ResultMerge[Label == "Malicious"])
 dim(redTeamUnique[Source_User %in% MalUsers])
@@ -115,6 +107,6 @@ ResultMerge[, DestUser:=NULL]
 ResultMerge[Source_User != Destination_User, SuspiciousAuth:= 1L]
 ResultMerge[Source_User == Destination_User, SuspiciousAuth:= 0L]
 
-ResultMerge[, names(ResultMerge)[c(2:9, 11, 13:14)]:= lapply(.SD, function(x) as.numeric(as.factor(x)) ), 
-.SDcols = c(2:9, 11, 13:14)]
+ResultMerge[, names(ResultMerge)[c(2:9, 11, 13:14)]:= 
+	    lapply(.SD, function(x) as.numeric(as.factor(x)) ), .SDcols = c(2:9, 11, 13:14)]
 fwrite(ResultMerge, "ResultAllUsers.csv", nThread = 40)
